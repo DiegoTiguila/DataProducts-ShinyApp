93d724394d0cf63302ca18d7c7b3dd14a43c0dfd
DiegoTiguila <diego.tiguila@gmail.com>
2014-06-24 01:03:47 -0600

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,9 +1,18 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-headerPanel("Body mass index calculator"),
+headerPanel(
+h1("Body mass index calculator", align = "center"),
+br()
+),
 sidebarPanel(
-numericInput('mass','Enter your mass in Kg', 0, min=0 , step=0.01),
-numericInput('height','Enter your height in m', 0, min=0, step=0.01),
+ p("The body mass index (BMI) is a measure of relative weight based on an individual's mass and height.
+    It is defined as the individual's body mass divided by the square of their height."),
+ br(),
+    p("The following application allows you to calculate your body mass index, for doing so you need to enter your weight(mass)
+     in kilograms and your height in meters in the panel below."),
+  br(),
+numericInput('mass','Enter your mass in Kg', 1, min=0 , step=0.01 ),
+numericInput('height','Enter your height in m', 1, min=0, step=0.01),
 submitButton('Submit')
 ),
 mainPanel(
