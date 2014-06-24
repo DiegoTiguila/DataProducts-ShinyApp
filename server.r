82eeb91608a2808dcf860077153e87b6f33249d6
DiegoTiguila <diego.tiguila@gmail.com>
2014-06-24 01:03:01 -0600

    Update server.r

--- server.r
+++ server.r
@@ -1,23 +1,38 @@
 library(shiny)
-BMI <- function(mass,height) round((mass/height),2)
+BMI <- function(mass,height) round((mass/height^2),2)
 BMIs <- function(BMI){
-  if(BMI<18.5){
-    status = "Underweight"
-  } else if(BMI<25){
-    status = "Normal"
-  } else if(BMI< 30){
-    status = "Overweight"
+  if(BMI < 15){
+    clasif = "Very severely underweight"
+  } 
+  if(BMI < 16){
+    clasif = "Severely underweight"
+  } 
+  if(BMI < 18.5){
+    clasif = "Underweight"
+  } else if(BMI < 25){
+    clasif = "Normal (healthy weight)"
+  } else if(BMI < 30){
+    clasif = "Overweight"
+  } else if(BMI < 35){
+    clasif = "Moderately obese"
+  } 
+  else if(BMI < 40){
+    clasif = "Severely obese"
   } else{
-    status = "Obese"
+    clasif = "Very severely obese"
   }
-  return(status)
+  return(clasif)
 }
 shinyServer(
 function(input, output) {
-BMI <- reactive({as.numeric(input$mass) /as.numeric(input$height)^2 })
+
 output$omass <- renderText({paste0("Mass: ",input$mass," kg")})
 output$oheight <- renderText({paste0("Height: ",input$height," m")})
-output$oBMI <- renderText({BMI(input$mass,input$height)})
-output$oBMIint <- renderText({BMIs(BMI)})
+output$oBMI <- renderText({paste0(BMI(input$mass,input$height)," m/kg^2")})
+
+output$oBMIint <- renderText({
+x<- BMI(input$mass,input$height)
+paste0("your category is: ",BMIs(x))
+})
 }
 )
